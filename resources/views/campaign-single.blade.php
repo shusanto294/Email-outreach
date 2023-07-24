891be2340245acadada013a4087fdfb6f5955414
Shusanto <shusanto294@gmail.com>
2023-07-25 01:25:31 +0600

    Replace tinymce editor with Summernote

--- resources/views/campaign-single.blade.php
+++ resources/views/campaign-single.blade.php
@@ -1,7 +1,10 @@
 @extends('theme')
 
 @section('head')
-<script src="https://cdn.tiny.cloud/1/snt0hcywpilrk80vrf5bcnfhs5gozuf0mt22xz8mawt5bf6x/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<!-- include jquery and summernote css/js -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 @endsection
 
 @section('content')
@@ -10,23 +13,17 @@
         <input type="text" name="campaignName" placeholder="Campaign Name" class="form-control mb-3" value="{{ $campaign->name }}" required>
         <p>Dynamic variables: [firstname] [company]</p>
         <input type="text" name="emailSubject" placeholder="Subject" class="form-control mb-3" value="{{ $campaign->subject }}" required>
-        <textarea name="emailBody" id="" cols="30" rows="10" class="form-control mb-3">{!! $campaign->body !!}</textarea>
+        <textarea id="summernote" name="emailBody" id="" cols="30" rows="10" class="form-control mb-3">{!! $campaign->body !!}</textarea>
         <button type="submit" class="btn btn-secondary mt-3">Update campaign</button>
     </form>
 @endsection
 
 @section('footer')
 <script>
-    tinymce.init({
-      selector: 'textarea',
-      plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
-      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-      tinycomments_mode: 'embedded',
-      tinycomments_author: 'Author name',
-      mergetags_list: [
-        { value: 'First.Name', title: 'First Name' },
-        { value: 'Email', title: 'Email' },
-      ]
+  $(document).ready(function() {
+    $('#summernote').summernote({
+      minHeight: 200
     });
-  </script>
+  });
+</script>
 @endsection
