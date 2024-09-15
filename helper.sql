97057c7f04cad8b1a3239c77a568ad00ff919af6
Shusanto Kumar Modak <shusanto294@gmail.com>
2024-09-15 16:45:39 +0600

    Lead updated increased to 500

--- helper.sql
+++
@@ -1,8 +0,0 @@
--- Insert new lead
-INSERT INTO `leads` (`id`, `name`, `linkedin_profile`, `title`, `company`, `company_website`, `location`, `email`, `campaign_id`, `sent`, `opened`, `technology`, `personalized_line`, `subscribe`, `created_at`, `updated_at`) VALUES (NULL, 'Tonmoy Sarkar', '#', 'CEO', 'Apple', 'example.com', 'USA', 'shusanto294@gmail.com', '12', '0', '0', NULL, NULL, '1', NULL, NULL);
-
--- Flush all existing leads
-UPDATE `leads` SET `email`='shusanto294@gmail.com'
-
--- Make all sent emails un opened
-UPDATE `emails` SET `opened`='0'
